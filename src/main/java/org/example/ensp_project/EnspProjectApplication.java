57cd775fa23b9646fae5715c95fc3e0b8b32d458
Emmanuel5495 <etraore21@gmail.com>
2026-06-28 11:59:40 +0000

    init: structure initiale du projet ENSP Scolarité

---
+++ src/main/java/org/example/ensp_project/EnspProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.ensp_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnspProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnspProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ensp_project/EnspProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ensp_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnspProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
